234784269d088241bfe2dc7241b3b205badf51e9
Stanislav Slip <kaopuk@gmail.com>
2024-04-22 21:38:06 +0300

    start api server

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-def main():
-    print("main function is now")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-if __name__ == "__main__":
-    main()
-    print("this is for new branch")
+class Task(BaseModel):
+    name: str
+    description: str | None
     
+
+app = FastAPI()
+
+@app.get("/home")
+def get_home():
+    return {"id": 1,
+            "data": "home page"}
+#
+# def main():
+#     print("main function is now")
+#
+#
+# if __name__ == "__main__":
+#     main()
+#     print("this is for new branch")
